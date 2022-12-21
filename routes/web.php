4eb59c78c555a476ed5d31f380d821e216c26b97
Bruno Vinicius <vruno.binicius@gmail.com>
2022-12-21 20:26:06 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -30,20 +30,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
 /**
  * Rotas de notas
  */
-Route::get('notas', [NotaController::class, 'index'])->name('notas.list');
-Route::get('notas/create', [NotaController::class, 'create'])->name('notas.create');
-Route::post('notas/create', [NotaController::class, 'store'])->name('notas.create.post');
-Route::get('notas/edit/{id}', [NotaController::class, 'show'])->name('notas.edit');
-Route::post('notas/edit/{id}', [NotaController::class, 'update'])->name('notas.edit.post');
-Route::get('notas/delete/{id}', [NotaController::class, 'destroy'])->name('notas.destroy.post');
+Route::get('/notas', [NotaController::class, 'index'])->name('notas.list');
+Route::get('/notas/create', [NotaController::class, 'create'])->name('notas.create');
+Route::post('/notas/create', [NotaController::class, 'store'])->name('notas.create.post');
+Route::get('/notas/edit/{id}', [NotaController::class, 'show'])->name('notas.edit');
+Route::post('/notas/edit/{id}', [NotaController::class, 'update'])->name('notas.edit.post');
+Route::get('/notas/delete/{id}', [NotaController::class, 'destroy'])->name('notas.destroy.post');
 
 /**
  * Rotas de usuários
  */
 Route::get('usuarios', [UsuarioController::class, 'index'])->name('usuarios.list');
+
+
+
 
 require __DIR__ . '/auth.php';
